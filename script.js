078ec02cc181e972d7717cf5f9c2b2fe0a54fc2f
Joao-Gabriel-Jms <joaogabrieljochims@gmail.com>
2026-03-13 04:02:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function filterColors() {
+    if (document.querySelector('.menu').style.display === 'none') {
+        document.querySelector('.menu').style.display = 'flex';
+    } else {
+        document.querySelector('.menu').style.display = 'none';
+    }
+
+  
+}
